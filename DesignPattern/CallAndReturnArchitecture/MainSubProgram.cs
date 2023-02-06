a41a5fee084d84a693807093adfa230926358865
Ling-yunchi <wr020210@gmail.com>
2023-02-06 16:40:25 +0800

    feat: Finish CallAndReturnArchitecture
    - finish CallAndReturnArchitecture
      - MainSubProgram
      - OOP

---
+++ DesignPattern/CallAndReturnArchitecture/MainSubProgram.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace DesignPattern.CallAndReturnArchitecture;
+
+public static class MainSubProgram
+{
+    public static int Data = 0;
+    [Test]
+    public static void Test() {
+        Console.WriteLine("Global Data: " + Data);
+        MainProgram();
+        Console.WriteLine("Global Data: " + Data);
+        Assert.AreEqual(4, Data);
+    }
+
+    private static void MainProgram() {
+        Data = 1;
+        SubProgram1();
+        Data += SubProgram2();
+    }
+
+    private static void SubProgram1() {
+        Data += 1;
+    }
+
+    private static int SubProgram2() {
+        return 2;
+    }
+}
---
+++ DesignPattern/CallAndReturnArchitecture/OOP.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace DesignPattern.CallAndReturnArchitecture;
+
+public class OOP
+{
+    public class Program
+    {
+        public int Data { get; private set; }
+
+        public Program() {
+            Data = 0;
+        }
+
+        public void MainProgram() {
+            Data = 1;
+            SubProgram1();
+            Data += SubProgram2();
+        }
+
+        private void SubProgram1() {
+            Data += 1;
+        }
+
+        private int SubProgram2() {
+            return 2;
+        }
+    }
+    
+    [Test]
+    public static void Test() {
+        var program = new Program();
+        Console.WriteLine("Data: " + program.Data);
+        program.MainProgram();
+        Console.WriteLine("Data: " + program.Data);
+        Assert.AreEqual(4, program.Data);
+    }
+}
